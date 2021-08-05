43b5a95bbf5a50142fb6456da59ae405d42b6004
Kush Gupta <74309241+iamKushGupta@users.noreply.github.com>
2021-08-05 14:42:33 +0530

    Create download.py

---
+++ download.py
@@ -0,0 +1,9 @@
+import requests
+
+def download(url):
+    get_response = requests.get(url)
+    file_name = url.split("/")[-1]
+    with open(file_name, "wb") as out_file:
+        out_file.write(get_response.content)
+
+download("https://yourwebsiteaddress.com/pagefiles/103952/1056x594/completeaddressoftargetfile.jpg")
